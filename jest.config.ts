32e850fc12dfa65a49323e13efeeb84b1329c8fa
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-04-10 20:11:10 -0300

    chore(gaqno-dropshipping-service): update

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,13 @@ const config: Config = {
   moduleFileExtensions: ["js", "json", "ts"],
   rootDir: "src",
   testRegex: ".*\\.spec\\.ts$",
-  transform: { "^.+\\.ts$": "ts-jest" },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      { tsconfig: "tsconfig.json", diagnostics: { ignoreCodes: [151002] } },
+    ],
+  },
+  moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
   collectCoverageFrom: ["**/*.ts", "!**/dto/**", "!main.ts"],
   coverageDirectory: "../coverage",
   testEnvironment: "node",
